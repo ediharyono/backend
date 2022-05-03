02e7b1e6ae8f09dcb6f14afacb8398407dff425f
Edi Haryono <energipositif@gmail.com>
2022-05-03 07:08:27 +0700

    Add files via upload

---
+++ basic01.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+ 
+//setting server port//
+app.listen(3100, () => {
+    console.log("server started on port");
+});
---
+++ basic02.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+app.use(cors({
+    origin: 'https://www.damaruta.com'
+}));
+
+    const ingredients = [
+    {
+        "id": "1",
+        "item": "Bacon"
+    },
+    {
+        "id": "2",
+        "item": "Eggs"
+    },
+    {
+        "id": "3",
+        "item": "Milk"
+    },
+    {
+        "id": "4",
+        "item": "Butter"
+    }
+];
+
+app.get('/ingredients', (req, res) =>{
+    res.send(ingredients);
+});
+app.listen(6069);
